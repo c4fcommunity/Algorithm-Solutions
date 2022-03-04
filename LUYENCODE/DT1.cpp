ee8809251e3ffa634d65b0da464d1963a75598d2
vietnammojang <73459857+vietnammojang@users.noreply.github.com>
2022-03-04 11:03:23 +0700

    up code

---
+++ LUYENCODE/DT1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double pi = 2 * acos(0);
+
+int main(){
+    double a;
+    cin >> a;
+    cout << fixed << setprecision(3) << (pi * pow(a, 2)) / 2;
+}
